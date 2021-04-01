292f522df41f0742c29d1bd88e4ea1256f1d21e3
Alex <alex@pop-os.localdomain>
2021-04-01 17:41:37 -0400

    fixed issues with question/answer matches

--- src/components/article-interview/article-interview.component.jsx
+++ src/components/article-interview/article-interview.component.jsx
@@ -9,12 +9,15 @@ import './article-interview.styles.scss'
 const ArticleInterview = ({ questions, answers }) => (
     <article className="article__container">
         {
-            questions.map((question, index) => (
-                <section className="article__q-a" key={question.number}>
-                    <PortableText className="article__question" content={question.answer}/>
-                    <PortableText className="article__answer" content={answers.find(answer => answer.number == question.number).answer} />
-                </section>
-            ))
+            questions.map((question) => (
+                // Check that a question and answer pair exist.
+                question.answer && answers.find(answer => answer.number == question.answer)?.answer) ? (
+                    <section className="article__q-a" key={question.number}>
+                        <PortableText className="article__question" content={question.answer}/>
+                        <PortableText className="article__answer" content={answers.find(answer => answer.number == question.number).answer} />
+                    </section>
+                ) : null
+            )
         }
     </article>
 )
