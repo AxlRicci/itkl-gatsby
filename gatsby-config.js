c4746e495dbe758fc0d47cd9eabc59550759fb20
Alex <alex@pop-os.localdomain>
2021-04-13 20:40:30 -0400

    Added Tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "process.env.GOOGLE_ANALYTICS_TRACKING_ID" || 'none',
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
         // Defines where to place the tracking script - `true`  in the head and `false` in the body
         head: true,
         // // Setting this parameter is optional
